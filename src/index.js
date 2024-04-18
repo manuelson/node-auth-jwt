d70c45d28c9fac3317e485c1a23f4fd0b2a1021d
Manu <manu.gonzalez@vicio.com>
2024-04-18 20:56:34 +0200

    Add https

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 const app = require('./app');
 const { logger } = require('./utils/logger');
+const https = require('https');
+const fs = require('fs');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    logger.info(`Running on PORT ${PORT}`);
+// Lee los archivos del certificado y la clave
+const privateKey = fs.readFileSync('/home/manu/sites/key.pem', 'utf8');
+const certificate = fs.readFileSync('/home/manu/sites/cert.pem', 'utf8');
+
+const httpsServer = https.createServer({ key: privateKey, cert: certificate }, app);
+
+httpsServer.listen(PORT, () => {
+    console.log('HTTPS Server running on port 3000');
 });
